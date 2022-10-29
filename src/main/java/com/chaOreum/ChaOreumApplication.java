0537afc5db3cfc5363ea413092b559e66b98ec42
TaeHeeKim <eksrnsdhkdwk@gmail.com>
2022-10-30 02:23:57 +0900

    [update] 배포를 위한 소스코드 추가 라이브러리 추가

--- src/main/java/com/chaOreum/ChaOreumApplication.java
+++ src/main/java/com/chaOreum/ChaOreumApplication.java
@@ -2,10 +2,17 @@ package com.chaOreum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ChaOreumApplication {
-
+public class ChaOreumApplication extends SpringBootServletInitializer {
+	//war 파일 배포를 위한 소스
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ChaOreumApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ChaOreumApplication.class, args);
 	}
